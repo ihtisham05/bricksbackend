2cc34ca847046ac085cd0644b1587bec9246a48d
unknown <ihtisham.javed@funavry.com>
2020-04-02 23:39:34 +0500

    first

---
+++ server/boot/autoMigration.js
@@ -0,0 +1,23 @@
+module.exports=function(app){
+    // var dataSource=app.dataSources.db;
+    // dataSource.autoupdate(null,function(err){
+    //     if (err) throw err;
+    // });
+};
+
+// module.exports=function(app){
+
+// 	var ds = app.datasources.db;
+// 	ds.autoupdate(null, function(err) {
+// 	  if (err) throw err;
+// 	  console.log('Finished migration');
+// 	  ds.disconnect();
+// 	});
+// };
+
+// module.exports = function(app){
+//     var dataSource = app.dataSources.db;
+//     dataSource.autoupdate(null, function(err) {
+//         if (err) return;
+//     });
+// };
